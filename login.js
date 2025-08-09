77480ea161259bbc9ceb843c0f9ef6f869a73280
bharathvaj68 <bharathvaj68@gmail.com>
2025-08-09 13:09:37 +0530

    user authentication added

--- login.js
+++ login.js
@@ -1,31 +1,83 @@
-// Your Supabase credentials
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-analytics.js";
+  import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
-import { createClient } from '@supabase/supabase-js'
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-const supabaseUrl = 'https://tuuxpejmdfdkhstduzeu.supabase.co'
-const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1dXhwZWptZGZka2hzdGR1emV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ2Nzc2NjAsImV4cCI6MjA3MDI1MzY2MH0.EzkEAO1gTUNtC46GopRf0oOCgMIfCFPY0xe5PxSsgY0
-const supabase = createClient(supabaseUrl, supabaseKey)
-document.getElementById("login").addEventListener("click", async (e) => {
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDG1Y7iwejRqS-aKBblxhSkKIyuS7mCBsE",
+    authDomain: "deupload-ebff4.firebaseapp.com",
+    projectId: "deupload-ebff4",
+    storageBucket: "deupload-ebff4.firebasestorage.app",
+    messagingSenderId: "1093150935544",
+    appId: "1:1093150935544:web:f83910a04c55b93ed7ae0d",
+    measurementId: "G-M5GR4YKZV7"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const auth = getAuth(app);
+
+
+  //sign-up button
+  const submit = document.getElementById("login");
+  submit.addEventListener("click", function(e){
     e.preventDefault();
+      
+    //inputs
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-    const email = document.getElementById("email").value.trim();
-    const password = document.getElementById("password").value.trim();
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Logged in 
+    const user = userCredential.user;
+    
+    window.location.href = "index.html";
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorMessage)
+    // ..
+  });
+  })
 
-    if (!email || !password) {
-        alert("Please fill in both fields.");
-        return;
-    }
 
-    // Login user in Supabase
-    const { data, error } = await supabase.auth.signInWithPassword({
-        email: email,
-        password: password
-    });
+// // Your Supabase credentials
 
-    if (error) {
-        alert("Login failed: " + error.message);
-    } else {
-        alert("Login successful!");
-        window.location.href = "index.html"; // redirect after login
-    }
-});
+// import { createClient } from '@supabase/supabase-js'
+
+// const supabaseUrl = 'https://tuuxpejmdfdkhstduzeu.supabase.co'
+// const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1dXhwZWptZGZka2hzdGR1emV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ2Nzc2NjAsImV4cCI6MjA3MDI1MzY2MH0.EzkEAO1gTUNtC46GopRf0oOCgMIfCFPY0xe5PxSsgY0
+// const supabase = createClient(supabaseUrl, supabaseKey)
+// document.getElementById("login").addEventListener("click", async (e) => {
+//     e.preventDefault();
+
+//     const email = document.getElementById("email").value.trim();
+//     const password = document.getElementById("password").value.trim();
+
+//     if (!email || !password) {
+//         alert("Please fill in both fields.");
+//         return;
+//     }
+
+//     // Login user in Supabase
+//     const { data, error } = await supabase.auth.signInWithPassword({
+//         email: email,
+//         password: password
+//     });
+
+//     if (error) {
+//         alert("Login failed: " + error.message);
+//     } else {
+//         alert("Login successful!");
+//         window.location.href = "index.html"; // redirect after login
+//     }
+// });
